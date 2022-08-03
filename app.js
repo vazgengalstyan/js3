6a655c2f06c2714af214998159131aca15a7487b
marine-israyelyan <mariisa19901412@gmail.com>
2022-08-03 19:36:17 +0300

    completed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert(5);
+let color =['red','green','yellow','blue']
+let elements = document.getElementsByClassName('box');
+let random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+let changeColor = ()=> {
+  for(let i =0; i < elements.length; i++){
+	let rand = random(0,4);
+	console.log(rand)
+	elements[i].style.backgroundColor = color[rand]
+}
+}
+setInterval(changeColor,600)
+
+
