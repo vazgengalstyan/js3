57f9011e7e9a6076c33d32eb33303cfe679abf8e
marine-israyelyan <mariisa19901412@gmail.com>
2022-08-06 16:32:50 +0300

    changed

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-let color =['red','green','yellow','blue']
-let elements = document.getElementsByClassName('box');
-let random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+/*js-ում ստանալ հղումները և ամեն երկրորդի գույնը դարձել կարմիր:*/
 
-let changeColor = ()=> {
-  for(let i =0; i < elements.length; i++){
-	let rand = random(0,4);
-	console.log(rand)
-	elements[i].style.backgroundColor = color[rand]
+let link = document.getElementsByClassName('url')
+let func = (link)=>{
+	for(let i = 0; i < link.length; i++){
+		if( i % 2  !== 0){
+			link[i].style.color = 'red'
+		}
+	}
 }
-}
-setInterval(changeColor,600)
+func(link)
 
+
+/*հղումներից ստանալ href-ները և լցնել մասիվի մեջ։  (մասիվը տպել):*/
+
+let getHref =(url)=>{
+	let arr =[];
+	for(let i = 0; i < url.length; i++){
+		arr.push(url[i].getAttribute('href'))
+	}
+	console.log(arr)
+}
+getHref(url)
+
+
+/*ջնջել href attribute-ը ամեն երկրորդ հղումից։*/
+
+let deleteHrefAttribute = (url) => {
+        for (let i = 0; i < url.length; i++) {
+                if (i % 2 !== 0) {
+                        url[i].removeAttribute('href')
+                }
+                console.log(url[i]);
+        }
+
+}
+
+deleteHrefAttribute(url);
 
